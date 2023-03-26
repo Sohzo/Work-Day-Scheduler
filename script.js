1298acfa88ef9e033e8cb37bfb7b8900174a0af4
Jonathan Winter <jonpw2004@outlook.com>
2023-03-26 17:28:51 -0600

    Cleaned up code and added comments

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 
-
+//Sets current date as variable
 var today = dayjs();
 
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
+  //This requires the function below to wait for all DOM elements to be loaded before running
   $(document).ready(function() {
 
+    //This function saves the info of the block into local storage when the save button is clicked
     $(".saveBtn").on("click", function() {
 
       var typedEvent = $(this).siblings(".description").val();
@@ -22,10 +17,12 @@ $(function () {
     })
   })
   
+  //This function sets the color of the blocks based on the current time
   function colorBlocks() {
 
     $(".time-block").each(function() {
       
+      //Formats the time to 24 hour clock
       var currentTime = today.format('HH');
       var compTime = parseInt($(this).attr("id"));
       
@@ -41,21 +38,16 @@ $(function () {
     });
   };
   
+  //This function takes the info from local storage and displays it on reload
   $(".hour").each(function() {
     var thisBlock = $(this).text();
     var returnEvent = localStorage.getItem(thisBlock);
-
-    console.log(returnEvent);
 
     $(this).siblings(".description").val(returnEvent);
     
   })
 
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  //This function sets the time under the header
   setInterval(function () {
     $('#currentDay').text(today.format('MMM D, YYYY hh:mm'));
   }, 1000);
